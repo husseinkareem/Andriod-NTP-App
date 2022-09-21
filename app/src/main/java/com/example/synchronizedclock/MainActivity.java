7e6f30207c9e013c314811111d66a6b154a9c273
husseinkareem <husseinkeldoon@yahoo.com>
2022-09-21 14:43:10 +0200

    Added comments.

--- app/src/main/java/com/example/synchronizedclock/MainActivity.java
+++ app/src/main/java/com/example/synchronizedclock/MainActivity.java
@@ -14,80 +14,80 @@ import java.util.Date;
 import org.apache.commons.net.ntp.NTPUDPClient;
 import org.apache.commons.net.ntp.TimeInfo;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {        // NTP server address
 
-    private boolean running = true;
-    private TextView UITxt;
-    private UIHandler UIhandler;
+    private boolean running = true;                          // Thread running flag
+    private TextView UITxt;                                  // UI text view
+    private UIHandler UIhandler;                             // UI handler
 
-    private static final String SERVER_NAME = "pool.ntp.org"; //Address to NTP server
+    private static final String SERVER_NAME = "pool.ntp.org";//Address to NTP server
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        Date date = new Date();
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        UITxt = (TextView) findViewById(R.id.Clock);
-        final Button updateUIBtn = findViewById(R.id.button);
-        updateUIBtn.setText("Connect");
-        UIhandler = new UIHandler();
+    protected void onCreate(Bundle savedInstanceState) {     //Create the activity
+        Date date = new Date();                              //Get the current date
+        super.onCreate(savedInstanceState);                  //Create the activity
+        setContentView(R.layout.activity_main);              //Set the layout
+        UITxt = (TextView) findViewById(R.id.Clock);         //Get the text view
+        final Button updateUIBtn = findViewById(R.id.button);//Get the button
+        updateUIBtn.setText("Connect");                      //Set the button text
+        UIhandler = new UIHandler();                         //Create the handler
 
-        updateUIBtn.setOnClickListener(new View.OnClickListener() {
+        updateUIBtn.setOnClickListener(new View.OnClickListener() {     //Button to start the clock
 
-            public void onClick(View v) {
-                if (updateUIBtn.getText() == "Connect") {
-                    UIThread thread = new UIThread();
-                    thread.start();
-                    updateUIBtn.setText("Disconnect");
-                    running = true;
+            public void onClick(View v) {                     //Start the clock
+                if (updateUIBtn.getText() == "Connect") {     //If the button is pressed
+                    UIThread thread = new UIThread();         //Start the thread
+                    thread.start();                           //Start the thread
+                    updateUIBtn.setText("Disconnect");        //Change the button text
+                    running = true;                           //Set the running variable to true
                 } else {
-                    updateUIBtn.setText("Connect");
-                    running = false;
+                    updateUIBtn.setText("Connect");           //Change the button text
+                    running = false;                          //Set the running variable to false
                 }
             }
         });
     }
-        public Date getCurrentNetworkTime() {
-            System.out.println("trying to set NTPClient");
-            NTPUDPClient timeClient = new NTPUDPClient();
-            timeClient.setDefaultTimeout(4000);
-            TimeInfo timeInfo;
+        public Date getCurrentNetworkTime() {                //Get the current time from the NTP server
+            System.out.println("trying to set NTPClient");   //Print to the console
+            NTPUDPClient timeClient = new NTPUDPClient();    //Create a new NTP client
+            timeClient.setDefaultTimeout(4000);              //Set the timeout to 4 seconds
+            TimeInfo timeInfo;                               //Create a new TimeInfo object
             try {
-                InetAddress inetAddress = InetAddress.getByName(SERVER_NAME);
-                timeInfo = timeClient.getTime(inetAddress);
-                long returnTime = timeInfo.getMessage().getTransmitTimeStamp().getTime();
-                Date date = new Date(returnTime);
-                System.out.println("NTPTime ");
-                return date;
-            } catch (IOException e) {
-                System.out.println("SystemTime ");
-                return new Date();
+                InetAddress inetAddress = InetAddress.getByName(SERVER_NAME);   //Get the address of the NTP server
+                timeInfo = timeClient.getTime(inetAddress);  //Get the time from the NTP server
+                long returnTime = timeInfo.getMessage().getTransmitTimeStamp().getTime();//Get the time from the NTP server
+                Date date = new Date(returnTime);           //Create a new date object
+                System.out.println("NTPTime ");             //Print to the console
+                return date;                                //Return the date
+            } catch (IOException e) {                       //If there is an error
+                System.out.println("SystemTime ");          //Print to the console
+                return new Date();                          //Return the current system time
             }
         }
-        private class UIHandler extends Handler {
+        private class UIHandler extends Handler {          //Create a new UIHandler
             @Override
-            public void handleMessage(Message msg) {
-                super.handleMessage(msg);
-                Bundle bundle = msg.getData();
-                String Time = bundle.getString("Time");
-                UITxt.setText(Time);
+            public void handleMessage(Message msg) {       //Handle the message
+                super.handleMessage(msg);                  //Handle the message
+                Bundle bundle = msg.getData();             //Get the bundle
+                String Time = bundle.getString("Time");  //Get the time from the bundle
+                UITxt.setText(Time);                       //Set the text of the text view to the time
             }
         }
-        protected class UIThread extends Thread {
+        protected class UIThread extends Thread {         //Create a new UIThread
             @Override
-            public void run() {
-                while (running) {
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
+            public void run() {                           //Run the thread
+                while (running) {                         //While the running variable is true
+                    try {                                 //Try to run the code
+                        Thread.sleep(1000);         //Sleep for 1 second
+                    } catch (InterruptedException e) {    //If there is an error
+                        e.printStackTrace();              //Print the error
                     }
-                    Message msg = new Message();
-                    Bundle bundle = new Bundle();
-                    String date = DateFormat.getTimeInstance(DateFormat.MEDIUM).format(getCurrentNetworkTime());
-                    bundle.putString("Time", date);
-                    msg.setData(bundle);
-                    MainActivity.this.UIhandler.sendMessage(msg);
+                    Message msg = new Message();          //Create a new message
+                    Bundle bundle = new Bundle();         //Create a new bundle
+                    String date = DateFormat.getTimeInstance(DateFormat.MEDIUM).format(getCurrentNetworkTime());//Get the current time
+                    bundle.putString("Time", date);       //Put the time in the bundle
+                    msg.setData(bundle);                  //Put the bundle in the message
+                    MainActivity.this.UIhandler.sendMessage(msg);//Send the message to the UIHandler
                 }
             }
         }
